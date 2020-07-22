dc074969dd81acfd8cac74786307fdb6df91d3e2
Chris <chriskl@live.com>
2020-07-22 15:20:25 +0800

    Added Post handler

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require("express");
 var path = require("path");
 var database = require("./db/db.json");
 var fs = require("fs");
+const { isBuffer } = require("util");
 
 
 var server = express();
@@ -23,14 +24,42 @@ server.get("/api/notes", function(req, res) {
     return res.json(tasks);
 });
 
+
+server.get("/api/notes:id", function(req, res) {
+    var addedNote = parseInt(req.params.id);
+
+    for (let key in notes) {}
+})
+
+
+
+
 server.post("/api/notes", function(req, res) {
     var newInfo = req.body;
     console.log(newInfo + " is gay");
 
     let topNote = 0;
     for (let key in tasks) {
-        let id = tasks
+        let id = tasks[key].id;
+        if (id > topNote) {
+            topNote = id;
+        }
     }
+
+    newInfo.id = topNote + 1;
+    tasks.push(newInfo);
+
+
+    fs.writeFile("db/db.json", JSON.stringify(tasks), function(err) {
+        if (err) {
+            return console.log(err);
+        }
+
+        console.log("Adding Task to Database");
+        res.json(newInfo);
+
+    });
+
 
 });
 
